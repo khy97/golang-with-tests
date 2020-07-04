144a7b767d0d38c0ce6f8b145b3eb1f192a1c620
Huiyeon Kim <gnldus59112@gmail.com>
2020-07-04 11:42:31 +0800

    Added name function

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -1,7 +1,12 @@
-package helloworld
+package main
 
 import "fmt"
 
+// Hello -> returns Hello world
+func Hello(name string) string {
+	return fmt.Sprintf("Hello, %s", name)
+}
+
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println(Hello("world"))
 }
